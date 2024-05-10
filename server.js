67a53afc7c04c365aa1a96c10d5648fc7d4da129
KevinKiely <Kevinkiely762@gmail.com>
2024-05-09 21:34:36 -0400

    Fixed all routes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const {
-    readFromFile,
-    readAndAppend,
-    writeToFile,
-  } = require('./helpers/fsUtils');
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('./helpers/fsUtils');
 
 
 
@@ -24,22 +24,22 @@ app.use(express.static('public'));
 
 // Homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET route for notes page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET api/notes should read db.json and return all saved notes as JSON
-app.get('/api/notes', (req, res) => { 
-    readFromFile("db/db.json").then((notes) => {
-        const dbJson= JSON.parse(notes);
-        res.json(dbJson);
+app.get('/api/notes', (req, res) => {
+  readFromFile("db/db.json").then((notes) => {
 
+    const dbJson = (JSON.parse(notes));
+    res.json(dbJson);
 
-    });
+  });
 });
 
 
@@ -49,64 +49,64 @@ app.get('/api/notes', (req, res) => {
 
 // POST request to add a new Note
 app.post('/api/notes', (req, res) => {
-    console.log(`${req.method} request received to add a new Note`);
-  
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if ( title && text ) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      // Obtain existing Notes
-      fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-          
-  
-          // Add a new review
-          parsedNotes.unshift(newNote);
-  
-            console.log(parsedNotes);
+  console.log(`${req.method} request received to add a new Note`);
 
-          // Write updated Notes back to the file
-          fs.writeFile('db/db.json',
-            JSON.stringify(parsedNotes, null),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.log('Successfully updated notes')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting review');
-    }
-  });
-  
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    // Obtain existing Notes
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+
+        // Add a new review
+        parsedNotes.unshift(newNote);
+
+        console.log(parsedNotes);
+
+        // Write updated Notes back to the file
+        fs.writeFile('db/db.json',
+          JSON.stringify(parsedNotes, null),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.log('Successfully updated notes')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
 
 // Catch
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // Live server at PORT 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
